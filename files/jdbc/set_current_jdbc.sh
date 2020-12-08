d39064ed48d3949305726ec4e3728f0aad4d4bcf
Aaron Dillow <adillow@expediagroup.com>
2020-12-07 20:42:26 -0800

    make set_current_jdbc.sh runnable from anywhere.

--- files/jdbc/set_current_jdbc.sh
+++ files/jdbc/set_current_jdbc.sh
@@ -1,5 +1,5 @@
 
-JDBC_DIRECTORY=$1
+JDBC_DIRECTORY=~/jdbc/$1
 CURRENT_JDBC=$2
 
 echo "Setting ${JDBC_DIRECTORY}/${CURRENT_JDBC} to be the current jdbc driver ${JDBC_DIRECTORY}/${JDBC_DIRECTORY}_current.jar."
